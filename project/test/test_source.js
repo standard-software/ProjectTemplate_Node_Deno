487e8a43f3865c85e28b0daeafacfa163deae901
standard-software <standard.software.net@gmail.com>
2020-06-22 00:01:52 +0900

    v1.1.1

--- project/test/test_source.js
+++ project/test/test_source.js
@@ -1,6 +1,6 @@
 import project from '../source/index.js'; // OK
 
-// import { test, test1, test2 } from '../source/index.js'; // OK
-// const project = { test, test1, test2 };
+// import { test, test1 } from '../source/index.js'; // OK
+// const project = { test, test1 };
 
 console.log(project.test(), project.test1());
